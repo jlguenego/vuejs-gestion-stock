e85bfbeaa692cc5f8c318b4551dd2f622d3fa010
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-28 15:48:56 +0200

    dev: added ts-node and use common

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,6 +1,7 @@
+import { Article } from "@gestionstock/common";
 import express from "express";
 
-const articles = [
+const articles: Article[] = [
   { id: "a1", name: "Tournevis", price: 2.99, qty: 200 },
   { id: "a2", name: "Pelle", price: 5, qty: 15 },
 ];
