0b604295770692ad00e6e5c5105b0fc6c77fe500
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-28 14:49:48 +0200

    dev: added the server code

---
+++ back/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const port = 3000;
+const wwwDir = ".";
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("req: ", req.url);
+  next();
+});
+
+app.use(express.static(wwwDir));
+app.use(serveIndex(wwwDir, { icons: true }));
+
+app.listen(port, () => {
+  console.log(`Server started with success on port ${port}.`);
+});
