8c8873f5883cd98558adeece25156e50df6ab980
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-29 12:31:06 +0200

    dev: added scss on footer and header

--- front/src/components/layout/FooterLayout.vue
+++ front/src/components/layout/FooterLayout.vue
@@ -3,3 +3,21 @@
     <a href="#"> Mentions Légales </a>
   </footer>
 </template>
+
+<style lang="scss" scoped>
+footer {
+  height: 3em;
+  background-color: #eee;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    text-decoration: inherit;
+    color: inherit;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
--- front/src/components/layout/HeaderLayout.vue
+++ front/src/components/layout/HeaderLayout.vue
@@ -6,3 +6,32 @@
     </a>
   </header>
 </template>
+
+<style lang="scss" scoped>
+header {
+  height: 3em;
+  background-color: #eee;
+  padding: 0.5em;
+  display: flex;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    gap: 0.5em;
+    text-decoration: inherit;
+    color: inherit;
+    &:hover {
+      text-decoration: underline;
+    }
+
+    img {
+      height: 2.5em;
+    }
+    span {
+      font-size: 1.3em;
+      font-weight: bold;
+    }
+  }
+}
+</style>
