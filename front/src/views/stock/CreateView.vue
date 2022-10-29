619edd1bfa863b751e1458dca36c74bf714fa260
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-29 16:09:33 +0200

    dev: handle form

--- front/src/views/stock/CreateView.vue
+++ front/src/views/stock/CreateView.vue
@@ -1,18 +1,25 @@
+<script lang="ts" setup>
+import type { NewArticle } from "@gestionstock/common";
+import { ref } from "vue";
+
+const newArticle = ref<NewArticle>({ name: "Truc", price: 0.01, qty: 1 });
+</script>
+
 <template>
   <main>
     <h1>Ajout d'un article</h1>
     <form>
       <label>
         <div>Nom</div>
-        <input type="text" />
+        <input type="text" v-model="newArticle.name" />
       </label>
       <label>
         <div>Prix</div>
-        <input type="text" />
+        <input type="number" v-model="newArticle.price" />
       </label>
       <label>
         <div>Quantité</div>
-        <input type="text" />
+        <input type="qty" v-model="newArticle.qty" />
       </label>
       <button
         class="primary"
@@ -22,5 +29,6 @@
         Ajouter
       </button>
     </form>
+    {{ newArticle }}
   </main>
 </template>
