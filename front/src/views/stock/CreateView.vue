ec52ad1f9fe892684f37d5b748b8e173c9d9cd3b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-30 11:01:36 +0100

    dev: form submit + useRouter programmatically

--- front/src/views/stock/CreateView.vue
+++ front/src/views/stock/CreateView.vue
@@ -1,33 +1,35 @@
 <script lang="ts" setup>
 import type { NewArticle } from "@gestionstock/common";
 import { ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
 
 const newArticle = ref<NewArticle>({ name: "Truc", price: 0.01, qty: 1 });
+
+const submit = async () => {
+  await router.push(route.matched[0].path);
+};
 </script>
 
 <template>
   <main>
     <h1>Ajout d'un article</h1>
-    <form>
+    <form @submit.prevent="submit">
       <label>
         <div>Nom</div>
         <input type="text" v-model="newArticle.name" />
       </label>
       <label>
         <div>Prix</div>
-        <input type="number" v-model="newArticle.price" />
+        <input type="number" v-model="newArticle.price" step="0.01" />
       </label>
       <label>
         <div>Quantité</div>
         <input type="qty" v-model="newArticle.qty" />
       </label>
-      <button
-        class="primary"
-        type="button"
-        @click="$router.push($route.matched[0].path)"
-      >
-        Ajouter
-      </button>
+      <button class="primary">Ajouter</button>
     </form>
     {{ newArticle }}
   </main>
