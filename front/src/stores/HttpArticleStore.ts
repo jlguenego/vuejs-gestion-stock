07ef35a872ea6fc73c0f77ec14ea7594a1e598f1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-31 15:12:04 +0100

    dev: added fetch api

--- front/src/stores/HttpArticleStore.ts
+++ front/src/stores/HttpArticleStore.ts
@@ -2,12 +2,17 @@ import { defineStore } from "pinia";
 import { computed } from "vue";
 import { useArticleStore } from "./ArticleStore";
 
+const url = "http://localhost:3000/api/articles";
+
 export const useHttpArticleStore = defineStore("http-article", () => {
   const articleStore = useArticleStore();
 
   const refresh = async () => {
     console.log("http refresh");
-    await articleStore.refresh();
+    const response = await fetch(url);
+    console.log("response: ", response);
+    const json = await response.json();
+    articleStore.articles = json;
   };
 
   return {
