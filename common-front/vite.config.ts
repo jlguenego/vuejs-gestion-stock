9777fbb30cc90614cdce49e86c9ab0a7ea424d60
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-01 15:37:32 +0100

    dev: added build library to vite config

--- common-front/vite.config.ts
+++ common-front/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/lib.ts"),
+      name: "CommonFront",
+      // the proper extensions will be added
+      fileName: "common-front",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
