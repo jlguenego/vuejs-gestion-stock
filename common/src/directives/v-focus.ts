ed48d27821f68bdb1ed285f900a152741e63bd82
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-01 10:47:59 +0100

    chore: remove console log

--- common/src/directives/v-focus.ts
+++ common/src/directives/v-focus.ts
@@ -4,7 +4,6 @@ type FocusValue = "selectall" | "focus";
 
 export const vFocus: Directive<HTMLElement, FocusValue | undefined> = {
   mounted: (el, binding, vnode, prevVnode) => {
-    console.log("binding: ", binding);
     const value = binding.value;
     if (value === "selectall" && el instanceof HTMLInputElement) {
       el.select();
