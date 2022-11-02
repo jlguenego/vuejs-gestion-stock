28c679a7d025fac996f4b8d1535610a9d9c70d8f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-02 13:31:06 +0100

    dev: common-front exposed as a plugin

--- common-front/src/lib.ts
+++ common-front/src/lib.ts
@@ -1,3 +1,10 @@
+import { App, Plugin } from "vue";
 import DynamicTitle from "./components/DynamicTitle.vue";
-export { DynamicTitle };
-export { vFocus } from "./directives/v-focus";
+import { vFocus } from "./directives/v-focus";
+
+const commonFrontPlugin = (app: App<Element>, options: {}) => {
+  app.directive("focus", vFocus);
+  app.component("dynamic-title", DynamicTitle);
+};
+
+export { DynamicTitle, vFocus, commonFrontPlugin };
--- front/src/main.ts
+++ front/src/main.ts
@@ -6,14 +6,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { FontAwesomeIcon } from "./icons/icons";
-import { DynamicTitle, vFocus } from "@gestionstock/common-front";
+import { commonFrontPlugin } from "@gestionstock/common-front";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.directive("focus", vFocus);
-app.component("dynamic-title", DynamicTitle);
+app.use(commonFrontPlugin, {});
 app.component("fa-icon", FontAwesomeIcon);
 
 app.mount("#app");
