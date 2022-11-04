2fdfb41adeab51e798940c08298ed3ee2c38382a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-04 19:13:24 +0100

    dev: functional component

--- front/src/components/ProjectVersion.tsx
+++ front/src/components/ProjectVersion.tsx
@@ -1,17 +1,10 @@
 import type { Component } from "vue";
 import { version } from "../../package.json";
 
-const ProjectVersion: Component = {
-  props: {
-    prefix: String,
-  },
-  setup(props) {
-    return () => (
-      <div>
-        jsx {props.prefix ?? "The version is now"} : {version}
-      </div>
-    );
-  },
-};
+const ProjectVersion: Component = (props: { prefix: String }) => (
+  <div>
+    jsx functional {props.prefix ?? "The version is now"} : {version}
+  </div>
+);
 
 export default ProjectVersion;
